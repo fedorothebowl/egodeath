527a3a3423e1e5ca6c83398f7e1ecd96753d7d31
federico petrucci <federico@quivi.it>
2024-06-21 16:40:06 +0200

    init

--- resources/js/carosello.js
+++ resources/js/carosello.js
@@ -1,17 +1,30 @@
 import Swiper from "swiper";
-import { Pagination } from 'swiper/modules';
+import { Pagination } from "swiper/modules";
 
-import 'swiper/css';
-import 'swiper/css/pagination';
-
+import "swiper/css";
+import "swiper/css/pagination";
 
 export default function () {
   return {
-    init() {
+    datas: undefined,
+    url: "/aforismi.json",
+
+    async init() {
+      const response = await fetch(this.url);
+
+      if (!response.ok) {
+        throw new Error("Errore nella richiesta!");
+      } else {
+        this.datas = await response.json();
+      }
+
       new Swiper(this.$el, {
+        slidesPerView: 1,
+        spaceBetween: 30,
         modules: [Pagination],
         pagination: {
           el: ".swiper-pagination",
+          dynamicBullets: true,
         },
       });
     },
